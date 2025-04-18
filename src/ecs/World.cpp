f126256ac4bd1f8cfceedae59ddb2323ab83b91d
halitipek <halitipek89@gmail.com>
2025-04-18 17:58:54 +0300

    feat: implement World.cpp

---
+++ src/ecs/World.cpp
@@ -0,0 +1,16 @@
+```cpp
+// File: src/ecs/World.cpp
+// Implementation of the empty World skeleton for the ECS library.
+// This file defines the default constructor and destructor.
+// (c) YourCompany 2024
+
+#include "ecs/World.h"
+
+namespace ecs {
+
+World::World() = default;
+
+World::~World() = default;
+
+} // namespace ecs
+```
